93e2b9b14ef41015c205b814d7a543d1e3afeb74
Eva María García Petrakova <102732975+ViiDost6@users.noreply.github.com>
2025-05-08 12:30:00 +0200

    weakpoints implementados y nuevo preset de enemigo

---
+++ Assets/Scripts/Characters/DialogueManager.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class DialogueManager : MonoBehaviour
+{
+    public static DialogueManager Instance;
+    public Canvas talkCanvas;
+    public Canvas dialogueCanvas;
+    public TextMeshProUGUI dialogueText;
+    public float typingSpeed = 0.05f;
+
+    private Coroutine dialogueRoutine;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        dialogueCanvas.gameObject.SetActive(false);
+        talkCanvas.gameObject.SetActive(false);
+    }
+
+    public void StartDialogue(string[] lines)
+    {
+        if (dialogueRoutine != null)
+            StopCoroutine(dialogueRoutine);
+
+        dialogueRoutine = StartCoroutine(ShowDialogue(lines));
+    }
+
+    IEnumerator ShowDialogue(string[] lines)
+    {
+        dialogueCanvas.gameObject.SetActive(true);
+        foreach (string line in lines)
+        {
+            dialogueText.text = "";
+            foreach (char letter in line)
+            {
+                dialogueText.text += letter;
+                yield return new WaitForSeconds(typingSpeed);
+            }
+
+            // Esperar a que el jugador presione R
+            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.R));
+        }
+
+        dialogueCanvas.gameObject.SetActive(false);
+        dialogueRoutine = null;
+    }
+}
+
